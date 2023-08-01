827075b523991ffb728b63fdcc055dfc1898fb80
huiyeong <81677222+almond-flavoured@users.noreply.github.com>
2023-08-01 17:58:04 +0900

    auth

---
+++ components/Auth/Form.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <div class="pt-5 space-y-6">
+      <UIInput value="asdkjflk" />
+    </div>
+  </div>
+</template>
+<script setup></script>
---
+++ components/UI/Input.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <label for=""></label>
+    <input type="text" :value="props.value" />
+  </div>
+</template>
+<script setup>
+const props = defineProps({
+  value: {
+    type: String,
+    required: true,
+  },
+});
+</script>
